f02599bfbe4652f1f52e77f8be102fd6d4380741
yuriarthurfs <yuriarthurf@gmail.com>
2025-09-21 16:37:17 -0300

    Updated supabaseParceiro.ts

--- src/lib/supabaseParceiro.ts
+++ src/lib/supabaseParceiro.ts
@@ -335,9 +335,10 @@ export const getFilterOptionsParceiro = async (filters: any = {}) => {
     // Busca níveis de aprendizagem únicos
 let padroesQuery = supabase
   .from('prova_resultados_parceiro')
-  .select('padrao_desempenho')
+  .select('padrao_desempenho', { distinct: true })
   .not('padrao_desempenho', 'is', null)
-  .not('padrao_desempenho', 'eq', '');
+  .not('padrao_desempenho', 'eq', '')
+  .limit(100000);
 
 
     // Busca habilidades únicas
