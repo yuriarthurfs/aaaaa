3bf8adc3a14b82a1be3aa6724792876d9c27dea5
yuriarthurfs <yuriarthurf@gmail.com>
2025-09-22 23:15:52 -0300

    Updated LevelTransitionChart.tsx

--- src/components/ComparativoSemestres/LevelTransitionChart.tsx
+++ src/components/ComparativoSemestres/LevelTransitionChart.tsx
@@ -7,7 +7,6 @@ interface LevelTransitionChartProps {
   selectedSystem: 'prova-parana' | 'parceiro';
 }
 
-
 const LevelTransitionChart: React.FC<LevelTransitionChartProps> = ({ data, selectedSystem }) => {
   const transitionData = React.useMemo(() => {
     // Agrupar por aluno e semestre
